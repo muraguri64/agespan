7eeda18e518c8eae91b16fac8e414155e5e32569
muraguri64 <muraguri64@gmail.com>
2018-08-11 13:30:41 +0300

    Add templates directory and media root and static root

--- agespan/settings.py
+++ agespan/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'agespan.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['agespan/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
