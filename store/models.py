b0df25bc10630bb62d377b7a9913c59ba07a867c
muraguri64 <muraguri64@gmail.com>
2018-12-15 15:03:43 +0300

    Alter Units field in store

--- store/models.py
+++ store/models.py
@@ -3,6 +3,14 @@ from regions.models import Region
 from crops.models import Crop_Type
 from django.contrib.auth.models import User
 
+UNITS_CHOICES = (
+    ('kg', 'Kg'),
+    ('boxes', 'Boxes'),
+    ('tray', 'Tray'),
+    ('net', 'Net'),
+    ('bunch', 'Bunch'),
+)
+
 # Create your models here.
 
 class Store_Product(models.Model):
@@ -15,7 +23,7 @@ class Store_Product(models.Model):
     nearest_town   = models.CharField(max_length=255)
     farm_size      = models.CharField(max_length=255)
     quantity       = models.IntegerField()
-    units          = models.CharField(max_length=255)
+    units          = models.CharField(max_length=255, choices=UNITS_CHOICES, default='Kg')
     unit_price     = models.CharField(max_length=255)
     image          = models.ImageField(upload_to='images/store')
     description    = models.TextField(blank=True, null=True)
