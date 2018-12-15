561d92420306353297571b52af1ea96b43838e23
muraguri64 <muraguri64@gmail.com>
2018-12-15 23:43:30 +0300

    Added the messages module and modified the sellproduct html template

--- store/views.py
+++ store/views.py
@@ -3,14 +3,16 @@ from django.http import HttpResponse
 
 from .forms import sell_Product_Form
 from .models import Store_Product, Order
+from django.contrib import messages
 
 # Create your views here.
 
 def sell_product_view(request):
-    form = sell_Product_Form(request.POST or None, request.FILES)
+    form = sell_Product_Form(request.POST or None, request.FILES or None)
     if form.is_valid():
         form.save()
         form = sell_Product_Form()
+        messages.success(request, 'Product submitted successfuly')
 
     context = {
         'form': form
