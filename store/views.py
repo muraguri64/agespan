95f48a86eec3f469c4f3fcb30209cb5d0528a220
muraguri64 <muraguri64@gmail.com>
2018-12-23 15:09:45 +0300

    Add login required to sell product view

--- store/views.py
+++ store/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .forms import sell_Product_Form
 from .models import Store_Product, Order
-from django.contrib import messages
+
 
 # Create your views here.
 
+@login_required(login_url="/accounts/login/")
 def sell_product_view(request):
     form = sell_Product_Form(request.POST or None, request.FILES or None)
     if form.is_valid():
