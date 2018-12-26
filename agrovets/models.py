9600ce2f4d4b6c82a0eeee3637c8359133bad22b
muraguri64 <muraguri64@gmail.com>
2018-12-26 14:54:10 +0300

    Add created_at and updated_at

--- agrovets/models.py
+++ agrovets/models.py
@@ -35,6 +35,9 @@ class Request_Product(models.Model):
 
     user              = models.ForeignKey(User, models.SET_NULL, blank=True, null=True) 
 
+    created_at     = models.DateTimeField(auto_now_add=True)
+    updated_at     = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return self.requestor_name
