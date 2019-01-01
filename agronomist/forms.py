162f273a4600a2a2fa34d377eba72358dd5ee2e9
muraguri64 <muraguri64@gmail.com>
2019-01-01 14:43:39 +0300

    Add agronomist components

--- agronomist/forms.py
+++ agronomist/forms.py
@@ -37,3 +37,38 @@ class register_Agronomist_Form(forms.ModelForm):
             'user',
         ]
 
+
+
+class request_Agronomist_Form(forms.ModelForm):    
+
+
+    class Meta:
+        model = Request_Agronomist
+        exclude = ('user',)
+
+        widgets = {    
+
+            'crop_type': forms.Select(attrs={'class': 'form-control', 'required': True}),
+            'region': forms.Select(attrs={'class': 'form-control', 'required': True}),
+            'location': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter specific Location', 'required': True}),
+            'requestor_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Requestor Name', 'required': True}),
+            'requestor_email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Requestor Email Address', 'required': True}),
+            'requestor_contact': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Requestor Contact Details', 'required': True}),                       
+            'nature_of_problem': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter Description', 'rows': 3, 'required': True}),
+           
+                    
+
+        }
+
+        fields = [            
+            
+            'crop_type',
+            'requestor_name',
+            'requestor_email',
+            'requestor_contact',
+            'region',
+            'location',         
+            'nature_of_problem',
+            'user',
+        ]
+
--- agronomist/views.py
+++ agronomist/views.py
@@ -4,8 +4,8 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 
-from .forms import register_Agronomist_Form
-from .models import Register_Agronomist
+from .forms import register_Agronomist_Form, request_Agronomist_Form
+from .models import Register_Agronomist, Request_Agronomist
 
 # Create your views here.
 
@@ -31,9 +31,36 @@ def register_agronomist_view(request):
 
     return render(request, 'agronomist/registeragronomist.html', context) 
 
+
+@login_required(login_url="/accounts/login/")
+def request_agronomist_view(request):  
+
+    form = request_Agronomist_Form(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        request_agronomist           = form.save(commit=False)
+        request_agronomist.user      = request.user
+        request_agronomist.save()
+
+        form = register_Agronomist_Form()
+        messages.success(request, 'Agronomist requested successfuly!!')
+        return redirect('request-agronomist')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'agronomist/requestagronomist.html', context) 
+
+
+
 def all_agronomists_view(request):
     agronomists = Register_Agronomist.objects
     return render(request, 'agronomist/allagronomists.html', {'agronomists': agronomists} )
+
+
+def all_requested_agronomists_view(request):
+    requested_agronomists = Request_Agronomist.objects
+    return render(request, 'agronomist/requestedagronomists.html', {'requested_agronomists': requested_agronomists} )
 
 
 
