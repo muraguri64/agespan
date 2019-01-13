170ce0e2fc22259a1b6749419528e3e0a909e84b
muraguri64 <muraguri64@gmail.com>
2019-01-13 12:23:23 +0300

    Add created_at and updated_at fields and make migrations

--- crops/models.py
+++ crops/models.py
@@ -4,7 +4,10 @@ from django.db import models
 
 class Crop_Type(models.Model):
     name        = models.CharField(max_length=255) 
-    description = models.TextField(blank=True, null=True)  
+    description = models.TextField(blank=True, null=True)
+
+    created_at     = models.DateTimeField(auto_now_add=True)
+    updated_at     = models.DateTimeField(auto_now=True)  
 
 
     def __str__(self):
