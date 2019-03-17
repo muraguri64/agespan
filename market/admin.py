3330e13fe4e644ea434d6c36ace201da551291a8
muraguri64 <muraguri64@gmail.com>
2019-03-17 15:35:07 +0300

    Add Market to Admin

--- market/admin.py
+++ market/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Market
 
 # Register your models here.
+
+class MarketInfoAdmin(admin.ModelAdmin):
+    list_display = ('crop_type', 'region')
+    list_filter = ('crop_type', 'region')
+    search_fields = ('crop_type', 'region')   
+
+admin.site.register(Market, MarketInfoAdmin)
+
+
