3e90d1cacd938e8dbea657e57a2c8b61274bfd7c
muraguri64 <muraguri64@gmail.com>
2019-06-30 20:28:27 +0300

    Addcrop details

--- crops/urls.py
+++ crops/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include, re_path
 
-from .views import all_crops_view
+from .views import all_crops_view, crop_detail_view
 
 
 urlpatterns = [
     path('', all_crops_view, name="all-crops"),
+    path('detail/<int:crop_id>', crop_detail_view, name="crop_detail"),
     
 
 
--- crops/views.py
+++ crops/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -11,3 +11,8 @@ from .models import Crop_Type
 def all_crops_view(request):
     crops = Crop_Type.objects
     return render(request, 'crops/all-crops.html', {'crops': crops} )
+
+
+def crop_detail_view(request, crop_id):
+    crop = get_object_or_404(Crop_Type, pk=crop_id)
+    return render(request, 'crops/crop_detail.html', {'crop':crop})
